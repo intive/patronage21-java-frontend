3df23e6e494475acf508e7d84caeddf16a882811
lzolkiewski <62542051+lzolkiewski@users.noreply.github.com>
2021-06-09 10:07:04 +0200

    IP2 - 790 - display login (#31)

--- src/components/UI/SearchInput.js
+++ src/components/UI/SearchInput.js
@@ -6,6 +6,8 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import SearchIcon from "@material-ui/icons/Search";
 import { makeStyles } from "@material-ui/core/styles";
+import { Tooltip } from "@material-ui/core";
+import Fade from "@material-ui/core/Fade";
 
 const useStyles = makeStyles({
   formControl: {
@@ -18,29 +20,36 @@ function SearchInput(props) {
 
   return (
     <FormControl className={classes.formControl} variant="outlined">
-      <OutlinedInput
-        value={props.value}
-        placeholder={props.placeholder}
-        onChange={props.handleChange}
-        type="search"
-        inputProps={{
-          "aria-label": props.ariaLabel,
-        }}
-        labelWidth={0}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              type="submit"
-              aria-label="search"
-              disableFocusRipple
-              disableRipple
-              style={{ backgroundColor: "transparent" }}
-            >
-              <SearchIcon />
-            </IconButton>
-          </InputAdornment>
-        }
-      />
+      <Tooltip
+        title={props.tooltipMessage}
+        arrow
+        TransitionComponent={Fade}
+        TransitionProps={{ timeout: 600 }}
+      >
+        <OutlinedInput
+          value={props.value}
+          placeholder={props.placeholder}
+          onChange={props.handleChange}
+          type="search"
+          inputProps={{
+            "aria-label": props.ariaLabel,
+          }}
+          labelWidth={0}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                type="submit"
+                aria-label="search"
+                disableFocusRipple
+                disableRipple
+                style={{ backgroundColor: "transparent" }}
+              >
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </Tooltip>
     </FormControl>
   );
 }
@@ -50,6 +59,7 @@ SearchInput.propTypes = {
   ariaLabel: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
+  tooltipMessage: PropTypes.string.isRequired,
 };
 
 SearchInput.defaultProps = {
