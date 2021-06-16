9a64daf62d78f1b96b448cdb84db31d54d23cdb1
lzolkiewski <lukaszolkiewski@gmail.com>
2021-06-16 15:38:38 +0200

    IP2-953 - change input variant
    some UX tweaks
    added placeholder in bio
    added labels for labelless inputs

--- src/components/User/Edit/EditableUserHeader.js
+++ src/components/User/Edit/EditableUserHeader.js
@@ -3,6 +3,7 @@ import { useRecoilValue, useRecoilState } from "recoil";
 import { userIsEditedState, userProperty } from "../../../state/atoms";
 import { TextField } from "@material-ui/core";
 import UserHeader from "../UI/UserHeader";
+import { USER_FIRST_NAME_LABEL, USER_LAST_NAME_LABEL } from "../../../config/Constants";
 
 function EditableUserHeader() {
   const edited = useRecoilValue(userIsEditedState);
@@ -14,14 +15,14 @@ function EditableUserHeader() {
     setMethod(event.target.value);
   };
 
-  const item = (property, setMethod) => {
-    return <TextField value={property} onChange={handleChange(setMethod)} />;
+  const item = (label, property, setMethod) => {
+    return <TextField value={property} variant={"outlined"}  label={label} onChange={handleChange(setMethod)} />;
   };
 
   return (
     <UserHeader
-      firstName={edited ? item(firstName, setFirstName) : firstName}
-      lastName={edited ? item(lastName, setLastName) : lastName}
+      firstName={edited ? item(USER_FIRST_NAME_LABEL, firstName, setFirstName) : firstName}
+      lastName={edited ? item(USER_LAST_NAME_LABEL, lastName, setLastName) : lastName}
       image={image}
       alignItems={edited ? "baseline" : "center"}
     />
